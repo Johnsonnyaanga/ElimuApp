70e49fd0f46b2c65e2d25304034f198fab927006
unknown <johnsonyaanga@gmail.com>
2020-03-11 09:23:44 +0300

    appbar

--- app/src/main/java/com/example/elimuapp/MainActivity.java
+++ app/src/main/java/com/example/elimuapp/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
+        setContentView(R.layout.activity_main);
+        Toolbar mytool = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(mytool);
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.bar_menu, menu);
+        return  true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id== R.id.search){
+            Toast.makeText(getApplicationContext(),"i am a search icon",Toast.LENGTH_SHORT).show();
+        }
+        else if(id==R.id.settings){
+            Toast.makeText(getApplicationContext(),"i am a setting icon",Toast.LENGTH_SHORT).show();
+        }
+        else if(id==R.id.settings){
+            Toast.makeText(getApplicationContext(),"i am a setting icon",Toast.LENGTH_SHORT).show();
+        }
+        else if(id==R.id.logout){
+            Toast.makeText(getApplicationContext(),"i am a logout stuff",Toast.LENGTH_SHORT).show();
+        }
+        return true;
+
     }
 }
+
+
