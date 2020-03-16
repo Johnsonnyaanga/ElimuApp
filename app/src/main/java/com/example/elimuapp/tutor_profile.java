2a766734fe084050255b0624c4ce14ed52b0d108
unknown <johnsonyaanga@gmail.com>
2020-03-16 10:33:17 +0300

    profile resource

---
+++ app/src/main/java/com/example/elimuapp/tutor_profile.java
@@ -0,0 +1,14 @@
+package com.example.elimuapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tutor_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor_profile);
+    }
+}
